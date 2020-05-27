df020be8c8d3506e5c17be63c7d722c605c8a1be
edupanov <edupanov@gmail.com>
2020-05-27 17:30:08 +0300

    add functional table

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,18 +1,31 @@
 class Dom {
   constructor(selector) {
-    this.$el =typeof selector === 'string' ?
-      document.querySelector(selector) :
-      selector
+    this.$el = typeof selector === 'string'
+      ? document.querySelector(selector)
+      : selector
   }
+
   html(html) {
     if (typeof html === 'string') {
-      this.$el.innerHTML = html;
+      this.$el.innerHTML = html
       return this
     }
     return this.$el.outerHTML.trim()
   }
+
+  text(text) {
+    if (typeof text === 'string') {
+      this.$el.textContent = text
+      return this
+    }
+    if (this.$el.tagName.toLowerCase() === 'input') {
+      return this.$el.value.trim()
+    }
+    return this.$el.textContent.trim()
+  }
+
   clear() {
-    this.html('');
+    this.html('')
     return this
   }
 
@@ -23,15 +36,22 @@ class Dom {
   off(eventType, callback) {
     this.$el.removeEventListener(eventType, callback)
   }
+
+  find(selector) {
+    return $(this.$el.querySelector(selector))
+  }
+
   append(node) {
     if (node instanceof Dom) {
       node = node.$el
     }
+
     if (Element.prototype.append) {
       this.$el.append(node)
     } else {
       this.$el.appendChild(node)
     }
+
     return this
   }
 
@@ -58,17 +78,40 @@ class Dom {
           this.$el.style[key] = styles[key]
         })
   }
-}
 
+  id(parse) {
+    if (parse) {
+      const parsed = this.id().split(':')
+      return {
+        row: +parsed[0],
+        col: +parsed[1]
+      }
+    }
+    return this.data.id
+  }
+
+  focus() {
+    this.$el.focus()
+    return this
+  }
+
+  addClass(className) {
+    this.$el.classList.add(className)
+  }
+
+  removeClass(className) {
+    this.$el.classList.remove(className)
+  }
+}
 
 export function $(selector) {
   return new Dom(selector)
 }
 
 $.create = (tagName, classes = '') => {
-  const el = document.createElement(tagName);
+  const el = document.createElement(tagName)
   if (classes) {
     el.classList.add(classes)
   }
   return $(el)
-};
+}
